92e8a7e0b8a330632fa9aad84f8146753f7d50e5
Shahzaib-Iqbal-1057 <shahzaibiqbal1057@gmail.com>
2022-10-30 02:01:28 +0500

    third commit

---
+++ Data.js
@@ -0,0 +1,92 @@
+import cr7 from "./cr7.webp";
+import react from "./learn_react.png";
+import bikes from "./biking.jpeg"
+
+const Data =  [
+    {
+        id: 1,
+        title: "Life Lessons with CR7",
+        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 136,
+        coverImg: cr7,
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Learn React",
+        description: "Interested in becoming a React Developer? For beginner and experienced developers alike, join us",
+        price: 125,
+        coverImg: react,
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Group Mountain Biking",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: bikes,
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norway",
+        openSpots: 3,
+    },
+
+    {
+        id: 1,
+        title: "Life Lessons with CR7",
+        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 136,
+        coverImg: cr7,
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Learn React",
+        description: "Interested in becoming a React Developer? For beginner and experienced developers alike, join us",
+        price: 125,
+        coverImg: react,
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Group Mountain Biking",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: bikes,
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norway",
+        openSpots: 3,
+    }
+
+    
+    
+  ]
+  
+  
+
+export default Data;
--- index.js
+++ index.js
@@ -1,63 +1,53 @@
 import React from "react";
-import ReactDom from "react-dom";
-import "./style.css"
-import insta from './insta_logo.png';
-import whatsapp from './whatsapp.png'
-import fb from './fb_logo.png'
-import twitter from './twitter.png';
-import cat from './cat.jpeg';
+import ReactDom from 'react-dom';
+import "./style.css";
+import Data from "./Data.js";
+import airbnb from "./air_bnb_logo.webp"
 
 
-
-const Info = () =>
-{
-    return (
-      <div className="info">
-        <img src = {cat} />
-        <h2>Carlos Monti</h2>
-        <p id = "gold">Frontend Developer</p>
-        <p id = "website">Meow.Website</p>
-        <div className="buttons">
-        <button id = "mail">Email</button>
-        <button id = "linkedin">LinkedIn</button>
-        </div>
-      </div>
-    )
-}
-
-const About = () =>
+const NavBar = () => 
 {
   return (
-    <div className="interests">
-      <h3>Interests</h3>
-      <p>
-        Mount Everest is the tallest mountain in the world.<br></br>
-        Pacific ocean is the largest ocean in the world.<br></br>
-        Asia is the biggest continent.<br></br>
-        There is a Lake Within an Island on a Lake Within an Island.<br></br>
-      </p>
+    <div className="logo">
+      <img src = {airbnb} alt = "airBnB logo"/>
     </div>
   )
 }
 
-const Footer = () =>
-{
+const Card = (props) => {
   return (
-    <div className="footer">
-      <img src  = {insta}/>
-      <img src  = {whatsapp}/>
-      <img src  = {fb}/>
-      <img src  = {twitter}/>
+    <div className="cards">
+      <img src={props.coverImg} alt = "project images" className="images"/>
+      <p>{props.stats.rating}({props.stats.reviewCount}).{props.location}</p>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+      <strong>From ${props.price} / Person</strong>
+      
     </div>
   )
 }
 
-const Element = <div className="container">
-  <Info/>
-  <About/>
-  <Footer/>
-</div>
+const array = Data.map(function(x) 
+{
+  return (
+    <Card  id = {x.id} title = {x.title} description = {x.description}
+    price = {x.price} coverImg = {x.coverImg} stats = {x.stats} location = {x.location}
+    openSpots  = {x.openSpots}/>
+  )
+})
+
+
+const Element = <>
+  <>
+    <NavBar/>
+    <div className="cards-bar">
+      {array}
+    </div>
+  </>
+</>
+
+  
+
 
 const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(Element);
-
+root.render(Element)
