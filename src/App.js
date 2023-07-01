3f767d4def0928b8177642946fde11f6676c8344
Shahzaib-Iqbal-1057 <shahzaibiqbal1057@gmail.com>
2023-07-02 02:27:47 +0500

    Added src folder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from './components/Navbar.js';
+import Meme from './components/Meme.js';
+
+const App = () => {
+    return (
+        <>
+            <Navbar/>
+            <Meme/>
+        </>
+    )
+}
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Navbar = () =>
+{
+  return (<div className='navbar'>
+    <h1>Meme Generator</h1>
+    <p>React Course Project</p>
+  </div>) 
+}
+export default Navbar;
